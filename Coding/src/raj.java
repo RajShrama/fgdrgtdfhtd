03a99b78fce5ac08c5cc528d7d87b15004e22847
dell <dell@DESKTOP-5EP85SF>
2021-02-04 17:23:52 +0530

    iyfyfyftyf

---
+++ Coding/src/raj.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class raj {
+public static void main(String[] args) {
+	List<String> ra=new ArrayList<String>();
+		ra.add("raj");
+		ra.add("Sharma");
+		System.out.println(ra);
+	}
+
+}
